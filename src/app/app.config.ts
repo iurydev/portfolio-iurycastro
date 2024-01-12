c77a3936cd484d2b1028aeeedcc699bcbed64442
Iury Rodrigues Castro <iurycastro@Iurys-MacBook-Air.local>
2024-01-11 23:53:32 -0300

    TASK: Adding transloco and i18n with  support for Portuguese and English.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@ngneat/transloco';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideTransloco({
+        config: {
+            availableLangs: ['en', 'pt-br'],
+            defaultLang: 'pt-br',
+            reRenderOnLangChange: true,
+            prodMode: !isDevMode(),
+            fallbackLang: 'en',
+        },
+        loader: TranslocoHttpLoader,
+    }),
+    provideAnimations()
+],
 };
